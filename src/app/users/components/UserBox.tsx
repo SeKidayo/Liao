ed15f0265be6fe09e54e0f8f29d2286e7db90bdd
seki <seki4649@163.com>
2025-01-19 21:39:04 +0800

    feat:add loading state

--- src/app/users/components/UserBox.tsx
+++ src/app/users/components/UserBox.tsx
@@ -1,41 +1,45 @@
-'use client';
+"use client";
 
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Avatar from "@/components/avatar/Avatar";
+import LoadingModal from "@/components/loading-modal/LoadingModal";
 
 interface UserBoxProps {
-  data: any
+  data: any;
 }
 
-const UserBox: React.FC<UserBoxProps> = ({
-  data
-}) => {
-  
+const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = useCallback(() => {
     setIsLoading(true);
 
-    axios.post('/api/conversations', {
-      userId: data.id
-    })
-    .then((response) => {
-      router.push(`/conversations/${response.data.id}`);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((response) => {
+        router.push(`/conversations/${response.data.id}`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [data, router]);
-  
 
   return (
-    <div
-      onClick={handleClick}
-      className="
+    <>
+      {
+        isLoading && (
+          <LoadingModal />
+        )
+      }
+      <div
+        onClick={handleClick}
+        className="
         w-full
         relative
         flex
@@ -48,36 +52,35 @@ const UserBox: React.FC<UserBoxProps> = ({
         transition
         cursor-pointer
       "
-    >
-      <Avatar
-        currentUser={data}
-      />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
+      >
+        <Avatar currentUser={data} />
+        <div className="min-w-0 flex-1">
           <div className="focus:outline-none">
-            <div
-              className="
+            <div className="focus:outline-none">
+              <div
+                className="
                 flex
                 justify-between
                 items-center
                 mb-1
               "
-            >
-              <p
-                className="
+              >
+                <p
+                  className="
                   text-sm
                   font-medium
                   text-gray-900
                 "
-              >
-                {data.name}
-              </p>
+                >
+                  {data.name}
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default UserBox;
---
+++ src/app/users/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingModal from "@/components/loading-modal/LoadingModal";
+
+const Loading = () => <LoadingModal />
+
+export default Loading;
